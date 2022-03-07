ad156920e8e110f77bc87f93c54fdf3a1ae6409d
Jan Paulus <janpaulus95@web.de>
2022-03-07 11:05:20 +0100

    fix: correct normalization

--- app/metrics/customsquad/evaluate_squad.py
+++ app/metrics/customsquad/evaluate_squad.py
@@ -12,22 +12,12 @@ from collections import Counter
 
 
 def normalize_answer(s):
-    """Lower text and remove punctuation, articles and extra whitespace."""
-
-    def remove_articles(text):
-        return re.sub(r"\b(a|an|the)\b", " ", text)
+    """Remove extra whitespace."""
 
     def white_space_fix(text):
         return " ".join(text.split())
 
-    def remove_punc(text):
-        exclude = set(string.punctuation)
-        return "".join(ch for ch in text if ch not in exclude)
-
-    def lower(text):
-        return text.lower()
-
-    return white_space_fix(remove_articles(remove_punc(lower(s))))
+    return white_space_fix(s)
 
 
 def get_precision_recall_f1(prediction, ground_truth, metric_type="f1"):
--- app/metrics/customsquadv2/evaluate_squad_v2.py
+++ app/metrics/customsquadv2/evaluate_squad_v2.py
@@ -64,23 +64,12 @@ def make_qid_to_has_ans(dataset):
 
 
 def normalize_answer(s):
-    """Lower text and remove punctuation, articles and extra whitespace."""
-
-    def remove_articles(text):
-        regex = re.compile(r"\b(a|an|the)\b", re.UNICODE)
-        return re.sub(regex, " ", text)
+    """Remove extra whitespace."""
 
     def white_space_fix(text):
         return " ".join(text.split())
 
-    def remove_punc(text):
-        exclude = set(string.punctuation)
-        return "".join(ch for ch in text if ch not in exclude)
-
-    def lower(text):
-        return text.lower()
-
-    return white_space_fix(remove_articles(remove_punc(lower(s))))
+    return white_space_fix(s)
 
 
 def get_tokens(s):
